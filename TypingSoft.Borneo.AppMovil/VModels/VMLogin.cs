81b604bffdb5b4ca08b07e591d583a4fd1237c73
Jose Balboa <JoseBalboa412@hotmail.com>
2025-04-11 09:54:24 -0600

    Archivos

---
+++ TypingSoft.Borneo.AppMovil/VModels/VMLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingSoft.Borneo.AppMovil.VModels
+{
+    class VMLogin
+    {
+    }
+}
