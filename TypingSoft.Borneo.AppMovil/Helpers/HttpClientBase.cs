9e6c5d6f9efd8fac2f1ec0a999321b4d600e90bb
yzyDRNX <edaniellriv@gmail.com>
2025-04-11 10:15:42 -0600

    api

--- TypingSoft.Borneo.AppMovil/Helpers/HttpClientBase.cs
+++ TypingSoft.Borneo.AppMovil/Helpers/HttpClientBase.cs
@@ -1,12 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TypingSoft.Borneo.AppMovil.Helpers
 {
-    class HttpClientBase
+    public class HttpClientBase : HttpClient
     {
+        #region Constructores
+        public HttpClientBase()
+        {
+            this.urlController = string.Empty;
+            this.UrlController = string.Empty;
+            this.BaseAddress = new Uri(UrlBaseWebApi);
+            this.InitHeaders();
+        }
+
+        public HttpClientBase(string urlController)
+        {
+            this.urlController = string.Empty;
+            this.UrlController = urlController;
+            this.InitHeaders();
+        }
+        #endregion
+
+        #region Propiedades        
+        public string UrlBaseWebApi { get; set; } = Helpers.Settings.UrlBaseAPI;
+
+        string urlController;
+        protected string UrlController
+        {
+            get => urlController;
+            set
+            {
+                urlController = !value.EndsWith("/") ? value + "/" : value;
+                UrlBaseWebApi += (!UrlBaseWebApi.EndsWith("/") ? "/" : string.Empty) + value;
+                BaseAddress = new Uri(UrlBaseWebApi);
+            }
+        }
+        #endregion
+
+        #region Procesamiento
+        void InitHeaders()
+        {
+            DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            DefaultRequestHeaders.Add("ZUMO-API-VERSION", "2.0.0");
+            DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue() { NoCache = true };
+        }
+
+        private (HttpStatusCode StatusCode, TResponse Content) ProcessResponse<TResponse>(HttpResponseMessage res)
+        {
+            if (res.IsSuccessStatusCode)
+            {
+                var s = res.Content.ReadAsStringAsync().Result;
+                var response = JsonConvert.DeserializeObject<TResponse>(s);
+                return (res.StatusCode, response);
+            }
+            else
+            {
+                try
+                {
+                    //Para peticiones Bad request, el body contiene una instancia de error, por lo que se trata de obtener
+                    var s = res.Content.ReadAsStringAsync().Result;
+                    var response = JsonConvert.DeserializeObject<TResponse>(s);
+                    return (res.StatusCode, response);
+                }
+                catch (Exception ex)
+                {
+                    var mensaje = ex.Message;
+                    return (res.StatusCode, default(TResponse));
+                }
+            }
+        }
+
+        public async Task<(HttpStatusCode StatusCode, TResponse Content)> CallGetAsync<TResponse>(string url)
+        {
+            try
+            {
+                var res = await GetAsync(url);
+                return ProcessResponse<TResponse>(res);
+            }
+            catch (Exception ex)
+            {
+                return (HttpStatusCode.BadRequest, default(TResponse));
+            }
+        }
+
+        public async Task<(HttpStatusCode StatusCode, TResponse Content)> CallPostAsync<TRequest, TResponse>(string url, TRequest req)
+        {
+            try
+            {
+                var x = JsonConvert.SerializeObject(req);
+                var res = await PostAsync(url, new StringContent(JsonConvert.SerializeObject(req), Encoding.UTF8, "application/json"));
+                return ProcessResponse<TResponse>(res);
+            }
+            catch (Exception ex)
+            {
+                var mensaje = ex.Message;
+                return (HttpStatusCode.BadRequest, default(TResponse));
+            }
+        }
+
+        public async Task<(HttpStatusCode StatusCode, TResponse Content)> CallPutAsync<TRequest, TResponse>(string url, TRequest req)
+        {
+            try
+            {
+                var res = await PutAsync(url, new StringContent(JsonConvert.SerializeObject(req), Encoding.UTF8, "application/json"));
+                return ProcessResponse<TResponse>(res);
+            }
+            catch
+            {
+                return (HttpStatusCode.BadRequest, default(TResponse));
+            }
+        }
+
+        public async Task<(HttpStatusCode StatusCode, TResponse Content)> CallDeleteAsync<TResponse>(string url)
+        {
+            try
+            {
+                var res = await DeleteAsync(url);
+                return ProcessResponse<TResponse>(res);
+            }
+            catch
+            {
+                return (HttpStatusCode.BadRequest, default(TResponse));
+            }
+        }
+        #endregion                
     }
 }
--- TypingSoft.Borneo.AppMovil/Helpers/Settings.cs
+++ TypingSoft.Borneo.AppMovil/Helpers/Settings.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TypingSoft.Borneo.AppMovil.Helpers
+﻿namespace TypingSoft.Borneo.AppMovil.Helpers
 {
-    class Settings
+    public class Settings
     {
+        public const string UrlBaseAPI = "https://localhost:7002/api";
+
     }
 }
