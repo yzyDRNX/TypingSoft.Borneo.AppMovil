5752b2f4c4bef8ad8a11b1e0610d8549e4cb34a3
yzyDRNX <edaniellriv@gmail.com>
2025-04-28 13:07:44 -0600

    ...

--- TypingSoft.Borneo.AppMovil/Helpers/Settings.cs
+++ TypingSoft.Borneo.AppMovil/Helpers/Settings.cs
@@ -2,7 +2,8 @@
 {
     public class Settings
     {
-        public const string UrlBaseAPI = "https://192.168.1.130:45455/api/";
+        public const string UrlBaseAPI = "https://192.168.1.133:45455/api/";
+
 
         public static string? UltimaRuta { get; set; }
 
