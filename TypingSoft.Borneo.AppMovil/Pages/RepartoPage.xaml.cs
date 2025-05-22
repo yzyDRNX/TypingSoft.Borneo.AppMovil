2b24c83c63518ed6edf7d7c4528d7baa290fe086
Fr4ncx-04 <jose.1993.fran@gmail.com>
2025-05-22 10:26:01 -0600

    PaginaReparto

--- TypingSoft.Borneo.AppMovil/Pages/RepartoPage.xaml.cs
+++ TypingSoft.Borneo.AppMovil/Pages/RepartoPage.xaml.cs
@@ -27,9 +27,52 @@ public partial class RepartoPage : ContentPage
             CrearViewModelManualmente();
         }
     }
-	
 
-     private void CrearViewModelManualmente()
+    private readonly HashSet<Guid> productosSeleccionados = new HashSet<Guid>();
+
+    private void OnAñadirProductoClicked(object sender, EventArgs e)
+    {
+        // Obtener el producto seleccionado del Picker
+        var productoSeleccionado = productosPicker.SelectedItem as Models.Custom.ProductosLista;
+
+        if (productoSeleccionado != null)
+        {
+            // Verificar si el producto ya está en la lista
+            if (productosSeleccionados.Contains(productoSeleccionado.Id))
+            {
+                // Mostrar un mensaje de alerta si el producto ya está en la lista
+                DisplayAlert("Advertencia", "El producto ya está en la lista.", "OK");
+                return;
+            }
+
+            // Añadir el producto al HashSet
+            productosSeleccionados.Add(productoSeleccionado.Id);
+
+            // Crear un nuevo Label para mostrar el nombre del producto seleccionado
+            var productoLabel = new Label
+            {
+                Text = productoSeleccionado.Producto, // Accede directamente a la propiedad 'Producto'
+                FontSize = 14,
+                TextColor = Colors.White,
+                Margin = new Thickness(0, 5, 0, 0)
+            };
+
+            // Añadir el Label al StackLayout
+            productosSeleccionadosStack.Children.Add(productoLabel);
+        }
+        else
+        {
+            // Mostrar un mensaje si no se seleccionó ningún producto
+            DisplayAlert("Advertencia", "Por favor, selecciona un producto antes de añadirlo.", "OK");
+        }
+    }
+
+    private async void OnConcluirClicked(object sender, EventArgs e)
+    {
+        //Navegacion a UtileriasPage
+        await Navigation.PushAsync(new UtileriasPage());
+    }
+    private void CrearViewModelManualmente()
     {
         var catalogosService = new Services.CatalogosService();
         var catalogosBL = new BL.CatalogosBL(catalogosService);
