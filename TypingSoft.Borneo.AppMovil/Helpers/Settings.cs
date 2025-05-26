ff625240d5b219d0c77deaebdb2fc63b523b4e39
Jose Balboa <JoseBalboa412@hotmail.com>
2025-05-26 11:29:38 -0600

    Arreglar red

--- TypingSoft.Borneo.AppMovil/Helpers/Settings.cs
+++ TypingSoft.Borneo.AppMovil/Helpers/Settings.cs
@@ -2,8 +2,11 @@
 {
     public class Settings
     {
+#if ANDROID
         public const string UrlBaseAPI = "http://10.0.2.2:5099/api/";
-
+#else
+         public const string UrlBaseAPI = "http://192.168.1.242:5099/api/";
+#endif
 
         public static string? UltimaDescripcionRuta { get; set; }
         public static Guid IdRuta { get; set; }
