9dc51861338f86c192fe4b0c5e3a587832bb96ce
Jose Balboa <JoseBalboa412@hotmail.com>
2025-05-27 13:38:36 -0600

    Fecha

--- TypingSoft.Borneo.AppMovil/Pages/UtileriasPage.xaml.cs
+++ TypingSoft.Borneo.AppMovil/Pages/UtileriasPage.xaml.cs
@@ -1,12 +1,18 @@
 namespace TypingSoft.Borneo.AppMovil.Pages;
+using TypingSoft.Borneo.AppMovil.VModels;
 
 public partial class UtileriasPage : ContentPage
 {
-	public UtileriasPage()
+    public string FechaGuardada { get; private set; }
+    public UtileriasPage()
 	{
 		InitializeComponent();
-	}
+        var vm = new UtileriasPageViewModel();
+        BindingContext = vm;
+        FechaGuardada = vm.FechaActual;
+    }
     VModels.CatalogosVM ViewModel => this.BindingContext as VModels.CatalogosVM;
+
     private async void Impresion(object sender, EventArgs e)
     {
         // Usar CustomNavigation para navegar a ClientePage
---
+++ TypingSoft.Borneo.AppMovil/VModels/UtileriasPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TypingSoft.Borneo.AppMovil.VModels
+{
+    public class UtileriasPageViewModel : INotifyPropertyChanged
+    {
+        private string _fechaActual;
+        public string FechaActual
+        {
+            get => _fechaActual;
+            set
+            {
+                _fechaActual = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public UtileriasPageViewModel()
+        {
+            FechaActual = DateTime.Now.ToString("dd-MM-yyyy");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
