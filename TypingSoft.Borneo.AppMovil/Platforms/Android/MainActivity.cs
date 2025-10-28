6e257cadbd2fab7522a887e9a902acfd29c2675a
Gerardo Lopez Ibañez <gli_8607@hotmail.com>
2025-10-28 16:43:59 -0600

    Icono,Splash

--- TypingSoft.Borneo.AppMovil/Platforms/Android/MainActivity.cs
+++ TypingSoft.Borneo.AppMovil/Platforms/Android/MainActivity.cs
@@ -4,7 +4,18 @@ using Android.OS;
 
 namespace TypingSoft.Borneo.AppMovil
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(
+         Label = "Borneo", // 👈 Nombre visible de la app
+         Theme = "@style/Maui.SplashTheme",
+         MainLauncher = true,
+         LaunchMode = LaunchMode.SingleTop,
+         ConfigurationChanges =
+             ConfigChanges.ScreenSize |
+             ConfigChanges.Orientation |
+             ConfigChanges.UiMode |
+             ConfigChanges.ScreenLayout |
+             ConfigChanges.SmallestScreenSize |
+             ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
