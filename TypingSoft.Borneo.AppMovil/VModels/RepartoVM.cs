c7bde8b03da6afd149c0322d6b6e4ca653e65279
Gerardo Lopez Ibañez <gli_8607@hotmail.com>
2025-12-09 11:29:36 -0600

    .

--- TypingSoft.Borneo.AppMovil/VModels/RepartoVM.cs
+++ TypingSoft.Borneo.AppMovil/VModels/RepartoVM.cs
@@ -52,7 +52,7 @@ namespace TypingSoft.Borneo.AppMovil.VModels
         [ObservableProperty]
         ObservableCollection<PreciosGeneralesLocal> listadoPreciosLocal = new();
 
-        public async Task CargarProductosDesdeLocal()
+        public async Task CargarProductosDesdeLocal(Guid IdClienteAsociado)
         {
             var productosLocales = await _localDb.ObtenerProductosAsync();
 
